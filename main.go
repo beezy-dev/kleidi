048bb8e93085f0326e69d3042593c62d60fafcae
romdalf <romdalf>
2024-04-01 11:41:24 +0200

    ''

--- main.go
+++ main.go
@@ -25,13 +25,14 @@ const (
 	socketTimeOut	= 5*time.Second
 )
 
+var kleidiVersion string
+
 func main() {
 	var (
 		listenAddr      = flag.String("listen-addr", "unix:///tmp/kleidi-kms-plugin.socket", "gRPC listen address.")
 		timeout         = flag.Duration("timeout", socketTimeOut, "gRPC timeout.")
 		providerService = flag.String("provider-service", "pkcs11", "KMS provider to connect to (pkcs11, vault).")
 		configFilePath  = flag.String("config-file-path", "/opt/softhsm/config.json", "SoftHSM config file pat.")
-		kleidiVersion   string
 	)
 
 	// parsing environment variables.
